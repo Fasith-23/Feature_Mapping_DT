605285c6ed5935ba3be18c2327eafc12e3420924
Fasith-23 <89504643+Fasith-23@users.noreply.github.com>
2023-12-10 01:32:12 +0530

    ppt

--- Delaunay_Triangulation/display.py
+++ Delaunay_Triangulation/display.py
@@ -1,55 +1,44 @@
 import cv2
 import shutil
 
-
-# Read the output file cinng vertices and edges
 with open("Feature_Matching_DT\Delaunay_Triangulation\output_vertices_edges_b.txt", "r") as file:
     lines = file.readlines()
 
 base_image = cv2.imread("Feature_Matching_DT\Fast_Detection\input2.jpeg")  # Replace with your existing image
 height, width = base_image.shape[:2]
-# Draw edges on the existing image
-# Parse vertices and edges data
+
 vertices = []
 edges = []
 for line in lines:
     if line.startswith("Vertex"):
         vertex_str = line.split("(")[1].split(")")[0]
         x, y, z = map(float, vertex_str.split(","))
-        vertices.append((int(x*width/10), int(y*height/10)))  # Assuming 2D po fr image display
+        vertices.append((int(x*width/10), int(y*height/10)))
     elif line.startswith("Edge"):
         start_index = line.index("[")
         end_index = line.index("]")
-
-        # Extract the substring containing the coordinates
         coordinates_line = line[start_index:end_index + 1]
         print(coordinates_line)
-        # Remove parentheses and split the coordinates into individual values
         coords = coordinates_line.replace('[', '').replace(']', '').split(', ')
 
-        # Extracting individual coordinates
         x1, y1, z1, x2, y2, z2 = map(float, coords)
         edges.append(((int(x1*width/10), int(y1*height/10)), (int(x2*width/10), int(y2*height/10))))  # Assuming 2D po fr image display
 
 copied_image = base_image.copy()
 
-# Draw edges on the copied image
 for edge in edges:
-    cv2.line(copied_image, edge[0], edge[1], (0, 255, 0), 1)  # Green lines, adjust thickness if needed
+    cv2.line(copied_image, edge[0], edge[1], (0, 255, 0), 1) 
 for vertex in vertices:
     cv2.circle(copied_image, vertex, 3, (255, 0, 0), -1)
-# Save the copied image with the triangulation
+
 shutil.copyfile("Feature_Matching_DT\Fast_Detection\input2.jpeg", "Feature_Matching_DT\Fast_Detection\output2.jpeg")  # Make a copy of the original image
-cv2.imwrite("Feature_Matching_DT\Fast_Detection\output2.jpeg", copied_image)  # Save the modified image
+cv2.imwrite("Feature_Matching_DT\Fast_Detection\output2.jpeg", copied_image) 
 
-
-# Display the copied image with the triangulation
 cv2.imshow("Delaunay Triangulation", copied_image)
 
 with open(r"Feature_Matching_DT\Delaunay_Triangulation\final_features.txt", "r") as file2:  # Replace with your second vertices file
     lines2 = file2.readlines()
 
-# Parse the second set of vertices data
 vertices2 = []
 for line in lines2:
     if line.startswith("Vertex"):
@@ -57,12 +46,10 @@ for line in lines2:
         x, y, z = map(float, vertex_str.split(","))
         vertices2.append((int(x*width/10), int(y*height/10)))  # Assuming 2D points for image display
 
-# Create another copy of the image for drawing the second set of vertices
 copied_image2 = base_image.copy()
 
-# Draw vertices from the second set on the second copied image
 for vertex in vertices2:
-    cv2.circle(copied_image2, vertex, 3, (255, 0, 0), -1)  # Blue circles for second set of vertices
+    cv2.circle(copied_image2, vertex, 3, (255, 0, 0), -1) 
 shutil.copyfile("Feature_Matching_DT\Fast_Detection\input1.jpeg", r"Feature_Matching_DT\Fast_Detection\features.jpeg")  # Make a copy of the original image
 cv2.imwrite(r"Feature_Matching_DT\Fast_Detection\features.jpeg", copied_image2) 
 
--- Fast_Detection/main.py
+++ Fast_Detection/main.py
@@ -1,28 +1,18 @@
 import cv2
 
-# read input image
 img = cv2.imread('Feature_Matching_DT\Fast_Detection\input2.jpeg')
-
-# convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Initiate FAST object with default values
 fast = cv2.FastFeatureDetector_create(40)
 
-# find the keypoints on image (grayscale)
 kp = fast.detect(gray,None)
 img2 = cv2.drawKeypoints(img, kp, None)
 height, width = img.shape[:2]
 scale_factor_x = 10 / width
 scale_factor_y = 10 / height
-# Scale down keypoints to fit within 100x100 grid
 
 for point in kp:
     point.pt = (point.pt[0] * scale_factor_x, point.pt[1] * scale_factor_y)
-# draw keypoints in image
 
-
-# Print all default params
 print("Threshold: ", fast.getThreshold())
 print("nonmaxSuppression: ", fast.getNonmaxSuppression())
 print("neighborhood: ", fast.getType())
